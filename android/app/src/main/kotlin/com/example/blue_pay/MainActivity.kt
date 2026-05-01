043f902799b83093e03f15a3fcce00fb4ac2fab1
syedmgraza <syedmgraza@gmail.com>
2026-05-01 23:40:59 +0530

    Starting up the project

---
+++ android/app/src/main/kotlin/com/example/blue_pay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.blue_pay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        appBar: AppBar(title: Text('Bluepay'),),
+        body: Container(
+
+        ),
+      )
+    );
+  }
+
+}
